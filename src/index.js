dd8aa60674ac17c7e2dc61b8616cd292de22003d
n-pontes <nuno06.np@gmail.com>
2025-04-22 14:01:31 +0200

    Add array for thr projects, constructor for the tasks and a function to add a new task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('test');
+// Stores all the projects 
+const myProjects = [];
+
+// Tasks constructor
+class Task {
+    constructor (title, description, dueDate, priority, notes, checkList) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checkList = checkList;
+    }
+};
+
+// Add new task
+const addNewTask = (title, description, dueDate, priority, notes, checkList) => {
+    const newTask = new Task(title, description, dueDate, priority, notes, checkList);
+    myProjects.push(newTask);
+};
+
+addNewTask('Workout', 'I have to workout', '21-20-2025', 'High', 'Dont forget', 'Check it');
+addNewTask('Workout', 'I have to workout', '21-20-2025', 'High', 'Dont forget', 'Check it');
+addNewTask('Workout', 'I have to workout', '21-20-2025', 'High', 'Dont forget', 'Check it');
+addNewTask('Workout', 'I have to workout', '21-20-2025', 'High', 'Dont forget', 'Check it');
+addNewTask('Workout', 'I have to workout', '21-20-2025', 'High', 'Dont forget', 'Check it');
+addNewTask('Workout', 'I have to workout', '21-20-2025', 'High', 'Dont forget', 'Check it');
+
+console.table(myProjects);
